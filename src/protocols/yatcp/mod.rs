829a433a3b48b40df53c1a29e2de8f1c0fe74ae9
Banyc <36535895+Banyc@users.noreply.github.com>
2022-05-24 15:07:57 +0800

    docs: packet format

--- src/protocols/yatcp/mod.rs
+++ src/protocols/yatcp/mod.rs
@@ -4,7 +4,7 @@ pub mod packet_hdr;
 // # Packet header
 //
 // ```text
-// 0       2   3   4   5   6       8 (BYTE)
+// 0       2               6         (BYTE)
 // +-------+---------------+
 // | rwnd  |     nack      |
 // +-------+---------------+
@@ -13,7 +13,7 @@ pub mod packet_hdr;
 // # Fragment header
 //
 // ```text
-// 0       2   3   4   5   6       8 (BYTE)
+// 0               4   5           8 (BYTE)
 // +---------------+---+
 // |      seq      |cmd|
 // +---------------+---+
@@ -24,3 +24,17 @@ pub mod packet_hdr;
 // |                               |
 // +-------------------------------+
 // ```
+//
+// # Packet structure
+//
+// ```text
+// [packet header]
+// ([fragment header (Ack)])
+// ([fragment header (Ack)])
+// ([fragment header (Push)] [body])
+// ([fragment header (Push)] [body])
+// ```
+//
+// # Invariants
+//
+// - `len` (`Push`) should not be `0`
