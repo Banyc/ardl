9223a3053a1e42f534e6ed5a1d5a9234bb2914a6
Banyc <36535895+Banyc@users.noreply.github.com>
2022-05-29 17:35:05 +0800

    docs: packet structure

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,40 +1,40 @@
+//! # Packet header
+//!
+//! ```text
+//! 0       2               6         (BYTE)
+//! +-------+---------------+
+//! | rwnd  |     nack      |
+//! +-------+---------------+
+//! ```
+//!
+//! # Fragment header
+//!
+//! ```text
+//! 0               4   5           8 (BYTE)
+//! +---------------+---+
+//! |      seq      |cmd|
+//! +---------------+---+
+//! |  len (Push)   |
+//! +---------------+---------------+
+//! |                               |
+//! |          DATA (Push)          |
+//! |                               |
+//! +-------------------------------+
+//! ```
+//!
+//! # Packet structure
+//!
+//! ```text
+//! (Packet header)
+//! (Fragment header of type Ack)*
+//! (Fragment header of type Ack)*
+//! ((Fragment header of type Push) (body))*
+//! ((Fragment header of type Push) (body))*
+//! ```
+//!
+//! # Invariants
+//!
+//! - `len` (`Push`) should not be `0`
+
 pub mod frag_hdr;
 pub mod packet_hdr;
-
-// # Packet header
-//
-// ```text
-// 0       2               6         (BYTE)
-// +-------+---------------+
-// | rwnd  |     nack      |
-// +-------+---------------+
-// ```
-//
-// # Fragment header
-//
-// ```text
-// 0               4   5           8 (BYTE)
-// +---------------+---+
-// |      seq      |cmd|
-// +---------------+---+
-// |  len (Push)   |
-// +---------------+---------------+
-// |                               |
-// |          DATA (Push)          |
-// |                               |
-// +-------------------------------+
-// ```
-//
-// # Packet structure
-//
-// ```text
-// [packet header]
-// ([fragment header (Ack)])
-// ([fragment header (Ack)])
-// ([fragment header (Push)] [body])
-// ([fragment header (Push)] [body])
-// ```
-//
-// # Invariants
-//
-// - `len` (`Push`) should not be `0`
